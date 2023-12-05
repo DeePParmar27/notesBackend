b54d499894998f613dfe60fc81fd534ab87c453f
DeePParmar27 <132197948+DeePParmar27@users.noreply.github.com>
2023-12-05 18:22:14 +0530

    Update db.js
    url change

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb+srv://deepparmar36611:l9rW7OiThAvUhyHy@notes.ehgf0tn.mongodb.net/?retryWrites=true&w=majority"
+const mongoURI = "mongodb+srv://deepparmar36611:VdMi8TKNqXiAwnWd@notes.mw0rx6y.mongodb.net/?retryWrites=true&w=majority"
 
 mongoose.set("strictQuery", false); // if not use throws error
 
